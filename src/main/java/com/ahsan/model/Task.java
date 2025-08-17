2962a02c965c5b99adbfbd6b78acc20166fdd4b3
Ahsan Shareef Mohammed <officialasmohammed06@gmail.com>
2025-08-17 08:19:22 -0400

    Added @Document(collection = 'Tasks') which is similar to @Entity @Table(name='Tasks')

---
+++ src/main/java/com/ahsan/model/Task.java
@@ -0,0 +1,24 @@
+package com.ahsan.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Document(collection = "Tasks")
+public class Task
+{
+    @Id
+    private String taskId;
+    private String description;
+    private String priority; //--- P1, P2, P3
+    private String assignee;
+    private int storyPoint; //--- 1 or 2 or 3
+
+}
